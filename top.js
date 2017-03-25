73d32a6a192d92cf07f20a2b263381a98e749ca7
haruka <mogeassai@gmail.com>
2017-03-25 13:45:14 +0000

    Update page

--- top.js
+++ top.js
@@ -3,7 +3,7 @@
     $(window).load(function() {
     $('#mc_embed_signup').css("display","none");
     $('#mc_embed_signup').delay(500).fadeIn("slow"); 
-    $('#mc_embed_signup').addclass('move');
+    $('#mc_embed_signup').addClass('move');
     
 /*    $('#mc_embed_signup').delay(1000).css(transform,translate(0,0));
 */    
