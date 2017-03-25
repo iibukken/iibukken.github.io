b557e0f0e3098d758266cc471983b51a7c341e3f
haruka <mogeassai@gmail.com>
2017-03-25 13:42:12 +0000

    Update page

---
+++ top.js
@@ -0,0 +1,11 @@
+
+  
+    $(window).load(function() {
+    $('#mc_embed_signup').css("display","none");
+    $('#mc_embed_signup').delay(500).fadeIn("slow"); 
+    $('#mc_embed_signup').addclass('move');
+    
+/*    $('#mc_embed_signup').delay(1000).css(transform,translate(0,0));
+*/    
+    });
+    
